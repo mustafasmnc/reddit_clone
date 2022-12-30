df7a334abdb580c8a124e3e9b4061d104af6162c
Mustafa <mustafasmnc@gmail.com>
2022-12-30 17:31:07 +0300

    Community List Flutter Drawer

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/features/home/drawers/community_list_drawer.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
+
+  void displayDrawer(BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -12,9 +17,13 @@ class HomeScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text(('Home')),
         centerTitle: false,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {},
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () => displayDrawer(context),
+            );
+          }
         ),
         actions: [
           IconButton(
@@ -29,6 +38,7 @@ class HomeScreen extends ConsumerWidget {
           )
         ],
       ),
+      drawer: const CommunityListDrawer(),
     );
   }
 }
