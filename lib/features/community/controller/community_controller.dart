1f98d7e1911e4d1e67212ab78424397c5753d568
Mustafa <mustafasmnc@gmail.com>
2023-01-01 20:36:53 +0300

    Editing Community

--- lib/features/community/controller/community_controller.dart
+++ lib/features/community/controller/community_controller.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/core/providers/storage_repository_provider.dart';
 import 'package:reddit_clone/core/utils.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/features/community/repository/community_repository.dart';
@@ -15,8 +18,12 @@ final userCommunitiesProvider = StreamProvider((ref) {
 final communityControllerProvider =
     StateNotifierProvider<CommunityController, bool>((ref) {
   final communityRepository = ref.watch(communityRepositoryProvider);
+  final storageRepository = ref.watch(storageRepositoryProvider);
   return CommunityController(
-      communityRepository: communityRepository, ref: ref);
+    communityRepository: communityRepository,
+    storageRepository: storageRepository,
+    ref: ref,
+  );
 });
 
 final getCommunityByNameProvider = StreamProvider.family((ref, String name) {
@@ -28,10 +35,14 @@ final getCommunityByNameProvider = StreamProvider.family((ref, String name) {
 class CommunityController extends StateNotifier<bool> {
   final CommunityRepository _communityRepository;
   final Ref _ref;
+  final StorageRepository _storageRepository;
   CommunityController(
-      {required CommunityRepository communityRepository, required Ref ref})
+      {required CommunityRepository communityRepository,
+      required Ref ref,
+      required StorageRepository storageRepository})
       : _communityRepository = communityRepository,
         _ref = ref,
+        _storageRepository = storageRepository,
         super(false);
 
   void createCommunity(String name, BuildContext context) async {
@@ -61,5 +72,43 @@ class CommunityController extends StateNotifier<bool> {
 
   Stream<Community> getCommunityByName(String name) {
     return _communityRepository.getCommunityByName(name);
+  }
+
+  void editCommunity({
+    required File? profileFile,
+    required File? bannerFile,
+    required BuildContext context,
+    required Community community,
+  }) async {
+    state = true;
+    if (profileFile != null) {
+      //communities/profile/memes
+      final res = await _storageRepository.storeFile(
+        path: 'communities/profile',
+        id: community.name,
+        file: profileFile,
+      );
+      res.fold(
+        (l) => showSnackbar(context, l.message),
+        (r) => community = community.copyWith(avatar: r),
+      );
+    }
+    if (bannerFile != null) {
+      //communities/banner/memes
+      final res = await _storageRepository.storeFile(
+        path: 'communities/banner',
+        id: community.name,
+        file: bannerFile,
+      );
+      res.fold(
+        (l) => showSnackbar(context, l.message),
+        (r) => community = community.copyWith(banner: r),
+      );
+    }
+
+    final res = await _communityRepository.editCommunity(community);
+    state = false;
+    res.fold((l) => showSnackbar(context, l.message),
+        (r) => Routemaster.of(context).pop());
   }
 }
--- lib/features/community/screens/edit_community_screen.dart
+++ lib/features/community/screens/edit_community_screen.dart
@@ -8,6 +8,7 @@ import 'package:reddit_clone/core/common/loader.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
 import 'package:reddit_clone/core/utils.dart';
 import 'package:reddit_clone/features/community/controller/community_controller.dart';
+import 'package:reddit_clone/models/community_model.dart';
 import 'package:reddit_clone/theme/palette.dart';
 
 class EditCommunityScreen extends ConsumerStatefulWidget {
@@ -41,8 +42,18 @@ class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
     }
   }
 
+  void save(Community community) {
+    ref.read(communityControllerProvider.notifier).editCommunity(
+          profileFile: profileFile,
+          bannerFile: bannerFile,
+          context: context,
+          community: community,
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(communityControllerProvider);
     return ref.watch(getCommunityByNameProvider(widget.name)).when(
           data: ((community) => Scaffold(
                 backgroundColor: Pallete.darkModeAppTheme.backgroundColor,
@@ -50,71 +61,78 @@ class _EditCommunityScreenState extends ConsumerState<EditCommunityScreen> {
                   title: const Text('Edit Community'),
                   centerTitle: false,
                   actions: [
-                    TextButton(onPressed: () {}, child: const Text('Save'))
+                    TextButton(
+                      onPressed: () => save(community),
+                      child: const Text('Save'),
+                    )
                   ],
                 ),
-                body: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 200,
-                        child: Stack(
+                body: isLoading
+                    ? const Loader()
+                    : Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 5),
+                        child: Column(
                           children: [
-                            InkWell(
-                              onTap: () => selectBannerImage(),
-                              child: DottedBorder(
-                                borderType: BorderType.RRect,
-                                radius: const Radius.circular(10),
-                                dashPattern: const [10.4],
-                                strokeCap: StrokeCap.round,
-                                color: Pallete.darkModeAppTheme.textTheme
-                                    .bodyText2!.color!,
-                                child: Container(
-                                  width: double.infinity,
-                                  height: 150,
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10)),
-                                  child: bannerFile != null
-                                      ? Image.file(bannerFile!)
-                                      : community.banner.isEmpty ||
-                                              community.banner ==
-                                                  Constants.bannerDefault
-                                          ? const Center(
-                                              child: Icon(
-                                              Icons.camera_alt_outlined,
-                                              size: 40,
-                                            ))
-                                          : Image.network(community.banner),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              bottom: 20,
-                              left: 20,
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(30),
-                                onTap: () => selectProfileImage(),
-                                child: profileFile != null
-                                    ? CircleAvatar(
-                                        backgroundImage:
-                                            FileImage(profileFile!),
-                                        radius: 30,
-                                      )
-                                    : CircleAvatar(
-                                        backgroundImage:
-                                            NetworkImage(community.avatar),
-                                        radius: 30,
+                            SizedBox(
+                              height: 200,
+                              child: Stack(
+                                children: [
+                                  InkWell(
+                                    onTap: () => selectBannerImage(),
+                                    child: DottedBorder(
+                                      borderType: BorderType.RRect,
+                                      radius: const Radius.circular(10),
+                                      dashPattern: const [10.4],
+                                      strokeCap: StrokeCap.round,
+                                      color: Pallete.darkModeAppTheme.textTheme
+                                          .bodyText2!.color!,
+                                      child: Container(
+                                        width: double.infinity,
+                                        height: 150,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: bannerFile != null
+                                            ? Image.file(bannerFile!)
+                                            : community.banner.isEmpty ||
+                                                    community.banner ==
+                                                        Constants.bannerDefault
+                                                ? const Center(
+                                                    child: Icon(
+                                                    Icons.camera_alt_outlined,
+                                                    size: 40,
+                                                  ))
+                                                : Image.network(
+                                                    community.banner),
                                       ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 20,
+                                    left: 20,
+                                    child: InkWell(
+                                      borderRadius: BorderRadius.circular(30),
+                                      onTap: () => selectProfileImage(),
+                                      child: profileFile != null
+                                          ? CircleAvatar(
+                                              backgroundImage:
+                                                  FileImage(profileFile!),
+                                              radius: 30,
+                                            )
+                                          : CircleAvatar(
+                                              backgroundImage: NetworkImage(
+                                                  community.avatar),
+                                              radius: 30,
+                                            ),
+                                    ),
+                                  )
+                                ],
                               ),
                             )
                           ],
                         ),
-                      )
-                    ],
-                  ),
-                ),
+                      ),
               )),
           error: (error, stackTrace) => ErrorText(errorText: error.toString()),
           loading: () => const Loader(),
