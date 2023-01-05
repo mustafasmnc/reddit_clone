d45281bad36bd7313ebceb5cecca5f38b37c7f42
Mustafa <mustafasmnc@gmail.com>
2023-01-05 18:11:13 +0300

    Bottom Nav Bar

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,12 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/constants/constants.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/features/home/delegates/search_community_delegate.dart';
 import 'package:reddit_clone/features/home/drawers/community_list_drawer.dart';
 import 'package:reddit_clone/features/home/drawers/profile_drawer.dart';
+import 'package:reddit_clone/theme/palette.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _page = 0;
 
   void displayDrawer(BuildContext context) {
     Scaffold.of(context).openDrawer();
@@ -16,9 +26,16 @@ class HomeScreen extends ConsumerWidget {
     Scaffold.of(context).openEndDrawer();
   }
 
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final user = ref.watch(userProvider)!;
+    final currentTheme = ref.watch(themeNotifierProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text(('Home')),
@@ -37,20 +54,29 @@ class HomeScreen extends ConsumerWidget {
                   context: context, delegate: SearchCommunityDelegate(ref));
             },
           ),
-          Builder(
-            builder: (context) {
-              return IconButton(
-                icon: CircleAvatar(
-                  backgroundImage: NetworkImage(user.profilePic),
-                ),
-                onPressed: () => displayEndDrawer(context),
-              );
-            }
-          )
+          Builder(builder: (context) {
+            return IconButton(
+              icon: CircleAvatar(
+                backgroundImage: NetworkImage(user.profilePic),
+              ),
+              onPressed: () => displayEndDrawer(context),
+            );
+          })
         ],
       ),
+      body: Constants.tabWidgets[_page],
       drawer: const CommunityListDrawer(),
       endDrawer: const ProfileDrawer(),
+      bottomNavigationBar: CupertinoTabBar(
+        activeColor: currentTheme.iconTheme.color,
+        backgroundColor: currentTheme.backgroundColor,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
+        ],
+        onTap: onPageChanged,
+        currentIndex: _page,
+      ),
     );
   }
 }
