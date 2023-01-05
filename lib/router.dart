e5d0a4c9bed0066e66cb8eb6dcfbb51cf544ae18
Mustafa <mustafasmnc@gmail.com>
2023-01-05 20:55:52 +0300

    Add Post Screen UI

--- lib/router.dart
+++ lib/router.dart
@@ -6,6 +6,8 @@ import 'package:reddit_clone/features/community/screens/create_community_screen.
 import 'package:reddit_clone/features/community/screens/edit_community_screen.dart';
 import 'package:reddit_clone/features/community/screens/mods_tools_screen.dart';
 import 'package:reddit_clone/features/home/screens/home_screen.dart';
+import 'package:reddit_clone/features/post/screens/add_post_screen.dart';
+import 'package:reddit_clone/features/post/screens/add_post_type_screen.dart';
 import 'package:reddit_clone/features/user_profile/screens/edit_profile_screen.dart';
 import 'package:reddit_clone/features/user_profile/screens/user_profile_screen.dart';
 import 'package:routemaster/routemaster.dart';
@@ -38,5 +40,8 @@ final loggedInRoute = RouteMap(routes: {
       ),
   '/edit-profile/:uid': (routeData) => MaterialPage(
         child: EditProfileScreen(uid: routeData.pathParameters['uid']!),
+      ),
+  '/add-post/:type': (routeData) => MaterialPage(
+        child: AddPostTypeScreen(type: routeData.pathParameters['type']!),
       ),
 });
